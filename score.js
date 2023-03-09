0ab1d45f248879aff91a4d0bbdc318888ed72e55
Cristiano Figueiró <figocris@gmail.com>
2023-03-09 11:28:47 -0300

    Update score.js

--- score.js
+++ score.js
@@ -1,16 +1,16 @@
 function score(){
   
-if (clock_global > "0" && clock_global < "1") {
+if (clock_global > "0:0" && clock_global < "1:0") {
    console.log("compasso 1");
  }
-if (clock_global > "3" && clock_global < "6") {
+if (clock_global > "3:0" && clock_global < "6:0") {
    console.log("compasso 2");
   region.start = 10; 
   region.end = 30;
   region.update("");
   region.playLoop();
    }
-if (clock_global > "10" && clock_global < "12") {
+if (clock_global > "10:0" && clock_global < "12:0") {
    console.log("compasso 3");
   region.start = 50; 
   region.end = 60;
